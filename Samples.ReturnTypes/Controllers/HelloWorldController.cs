19d6041c3bc79f1f5ab00cff43d1a7814a54c6a8
Fabian Diaz <Fabian.Diaz@endava.com>
2019-04-24 15:03:27 -0500

    Update controller

--- Samples.ReturnTypes/Controllers/HelloWorldController.cs
+++ Samples.ReturnTypes/Controllers/HelloWorldController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Samples.ReturnTypes.Models;
 
@@ -12,18 +13,21 @@ namespace Samples.ReturnTypes.Controllers
     public class HelloWorldController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(string[]), StatusCodes.Status200OK)]
         public IEnumerable<string> Get()
         {
             return new string[]{ "Hello", "World"};
         }
 
         [HttpGet("IActionResult")]
+        [ProducesResponseType(typeof(string[]), StatusCodes.Status200OK)]
         public IActionResult GetIActionResult()
         {
             return this.Ok(new string[]{ "Hello", "World"});
         }
 
         [HttpPost("test")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult TestPost([FromBody]MyModel model){
             return this.Ok();
         }
